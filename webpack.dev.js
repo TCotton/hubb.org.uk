303b1c64de0c6ef1cb704b0ca0c2196e6a91f6b9
Andy Walpole <me@andywalpole.me>
2020-07-18 12:09:50 +0100

    basic webpack dev server config

---
+++ webpack.dev.js
@@ -0,0 +1,34 @@
+require('html-webpack-plugin');
+const HtmlWebpackPlugin =require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+
+  // https://webpack.js.org/concepts/entry-points/#multi-page-application
+  entry: {
+    index: './src/index.js'
+  },
+
+  // https://webpack.js.org/configuration/dev-server/
+  devServer: {
+    port: 8080,
+    publicPath: 'src',
+    open: true, 
+    openPage: 'src/index.html' 
+  },
+
+  output: {
+    publicPath: "./src",
+    path: path.resolve(__dirname, './src')
+    },
+
+  // https://webpack.js.org/concepts/plugins/
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+      inject: true,
+      chunks: ['index'],
+      filename: 'index.html'
+    })
+  ]
+};
