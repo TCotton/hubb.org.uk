9a0022c1c05a3fc821d1fc1331b1e4c3d0a2e685
Andy Walpole <me@andywalpole.me>
2020-07-19 13:32:56 +0100

    accessible html markup

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,36 +1,44 @@
-require('html-webpack-plugin');
-const HtmlWebpackPlugin =require('html-webpack-plugin');
-const path = require('path');
+require("html-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require("path");
 
 module.exports = {
-
   // https://webpack.js.org/concepts/entry-points/#multi-page-application
   entry: {
-    index: './src/index.js'
+    index: "./src/index.js",
   },
 
   // https://webpack.js.org/configuration/dev-server/
   devServer: {
     port: 8080,
-    publicPath: 'src',
-    open: true, 
-    openPage: 'src/index.html',
+    publicPath: "./src",
+    open: true,
+    openPage: "src/index.html",
     inline: true,
-    hot: true
+    hot: true,
   },
 
   output: {
     publicPath: "./src",
-    path: path.resolve(__dirname, './src')
-    },
+    path: path.resolve(__dirname, "./src"),
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
 
   // https://webpack.js.org/concepts/plugins/
   plugins: [
     new HtmlWebpackPlugin({
-      template: './src/index.html',
+      template: "./src/index.html",
       inject: true,
-      chunks: ['index'],
-      filename: 'index.html'
-    })
-  ]
+      chunks: ["index"],
+      filename: "index.html",
+    }),
+  ],
 };
