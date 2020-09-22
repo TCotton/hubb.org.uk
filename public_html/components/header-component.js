953bfe145a205507ae98001c9caab687d336b3ff
Andy Walpole <me@andywalpole.me>
2020-09-22 22:20:25 +0100

    new public html files

--- public_html/components/header-component.js
+++ public_html/components/header-component.js
@@ -8,7 +8,7 @@ class Header extends HTMLElement {
     <div class="header-bottom">
       <div class="leftheader">
         <h1>Hubb support group</h1>
-        <p class="tagtitle" role="doc-subtitle">'Helping Us Bounce Back'</p>
+        <p class="tagtitle" role="doc-subtitle">'Helping Users Bounce Back'</p>
         <img src="images/logo.png" alt="hub logo" class="hub-logo"/>
       </div>
       <div class="rightheader">
